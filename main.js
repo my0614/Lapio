54585bfc5463306e813e9054a73fd3cb55b59bce
kim min young <aqi222@naver.com>
2019-08-24 22:23:33 +0900

    node.js_module
    node.js 모듈파일 이용해서 출력하기

---
+++ main.js
@@ -0,0 +1,3 @@
+var util = require('util');
+var multi = util.format('%d * %d = %d', 150,3,150*3);
+console.log(multi);
---
+++ module.js
@@ -0,0 +1,6 @@
+exports.plus= function (n,m) {
+    return n+m;
+}
+    exports.multi = function (n,m) {
+        return n*m;
+};
